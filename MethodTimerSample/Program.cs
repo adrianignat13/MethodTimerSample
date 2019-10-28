e6059b5bf4a200df7fe1cd0c1747213c15b944d1
adrian.ignat <adrian.ignat@uipath.com>
2019-10-28 16:50:12 +0000

    Add async method that throws exception

---
+++ MethodTimerSample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using MethodTimer;
+
+namespace MethodTimerSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                AsyncDelay().Wait();
+                
+            }
+            catch
+            {
+
+            }
+        }
+
+        [Time]
+        public static async Task AsyncDelay()
+        {
+            await Task.Delay(1000);
+
+            throw new Exception();
+        }
+
+        public static async Task AsyncDelayWithTimer()
+        {
+            var sw = Stopwatch.StartNew();
+
+            try
+            {
+                await Task.Delay(1000);
+                throw new Exception();
+            }
+            finally
+            {
+                sw.Stop();
+                Trace.WriteLine($"Program.AsyncDelayWithTimer {sw.Elapsed}ms");
+            }
+
+        }
+
+    }
+}
